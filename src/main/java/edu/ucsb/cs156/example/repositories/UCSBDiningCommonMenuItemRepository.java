30fff95431e95f93976cfd53d4b1739ce7f1585e
Tony Tran <tbt@ucsb.edu>
2024-04-25 17:58:18 -0700

    added entity and repository for menuitems

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonMenuItemRepository.java
@@ -0,0 +1,11 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonMenuItems;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UCSBDiningCommonMenuItemRepository extends CrudRepository<UCSBDiningCommonMenuItems, Long> {
+}
